e693dab26ca4b3a61dc3046bfcf034e8caaffd91
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2023-10-24 01:45:48 +0800

    placed an error for wrong password or email in signin

--- public/js/hide-warning.js
+++ public/js/hide-warning.js
@@ -2,28 +2,49 @@ let password = "";
 let repassword = "";
 		
 
-    function handleOnKeyUp(input, event) {
+    const signinError = () => {
+        const warning = document.querySelector('.signin-warning');
+        warning.classList.remove('hide-warning');
+        document.querySelector(`.usernameSi`).addEventListener('keyup', (e) => {
+            toggleCall(e);
+        });
+        document.querySelector(`.emailSi`).addEventListener('keyup', (e) => {
+            toggleCall(e);
+        });
+        document.querySelector(`.passwordSi`).addEventListener('keyup', (e) => {
+            toggleCall(e);
+        });
+    }
+
+    const toggleCall = (event) => {
+        const currentKeys = event;
+        let currentKeysUp = '';
+        currentKeysUp = currentKeysUp ? currentKeysUp + currentKeys : currentKeys;
+        if (currentKeysUp.length != 0) { warningToggle(true, 'signin-warning') }
+    }
+
+    function handleOnKeyUp (input, event) {
         const currentKeys = document.querySelector(`#${input}`);
         const button = document.querySelector('.submit');
         let currentKeysUp = '';
         currentKeysUp = currentKeysUp ? currentKeysUp + currentKeys : currentKeys;
 		if (input === "repassword") { repassword = currentKeysUp.value } else { password = currentKeysUp.value }
 
-		if (repassword.length == 0) { console.log("waiting"); button.disabled = true; warningToggle(true); } else {
+		if (repassword.length == 0) { console.log("waiting"); button.disabled = true; warningToggle(true, 'warning'); } else {
 			if (password === repassword) {
                 button.disabled = false;
-                warningToggle(true);
+                warningToggle(true, 'warning');
 			} else {
 				console.log("no");
                 button.disabled = true;
-                warningToggle(false);
+                warningToggle(false, 'warning');
 			}
 		}
                     
     }
 
-    function warningToggle(status) {
-        const warning = document.querySelector('.warning');
+    function warningToggle(status, popup) {
+        const warning = document.querySelector(`.${popup}`);
         if(status){
             if(!warning.classList.contains('hide-warning')){
                 warning.classList.add('hide-warning');
