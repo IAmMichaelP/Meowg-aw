44a8d24713a6935e7029965d6c43e134f1ceefe0
Kzlyr Shaira Manejo <kmanejo@up.edu.ph>
2024-04-04 02:44:42 +0800

    updating front-end

--- public/assets/js/userscript.js
+++ public/assets/js/userscript.js
@@ -52,8 +52,7 @@ buttons.forEach(button => {
 
 // script for user-profile
 function showContent(section) {
-    const buttons = document.querySelectorAll('.user-profile-btns');
-    
+
     document.getElementById('profile-blog-content').style.display = 'none';
     document.getElementById('profile-submission-content').style.display = 'none';
     document.getElementById('profile-cart-content').style.display = 'none';
@@ -69,3 +68,33 @@ function showContent(section) {
         document.getElementById('profile-favorites-content').style.display = 'block';
     }
 }
+
+// admin-dashboard buttons
+function showDashContent(section) {
+    
+    document.getElementById('users').style.display = 'none';
+    document.getElementById('strays').style.display = 'none';
+    document.getElementById('donations').style.display = 'none';
+    document.getElementById('blogs').style.display = 'none';
+    document.getElementById('shop').style.display = 'none';
+    document.getElementById('faqs').style.display = 'none';
+    document.getElementById('messages').style.display = 'none';
+
+    if (section === 'users') {
+        document.getElementById('users').style.display = 'block';
+    } else if (section === 'strays') {
+        document.getElementById('strays').style.display = 'block';
+    } else if (section === 'donations') {
+        document.getElementById('donations').style.display = 'block';
+    }else if (section === 'blogs') {
+        document.getElementById('blogs').style.display = 'block';
+    }else if (section === 'shop') {
+        document.getElementById('shop').style.display = 'block';
+    }else if (section === 'faqs') {
+        document.getElementById('faqs').style.display = 'block';
+    }else if (section === 'messages') {
+        document.getElementById('messages').style.display = 'block';
+    }
+
+}
+
