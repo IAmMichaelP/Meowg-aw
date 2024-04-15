ce7d3198e109b3a5721bcf35055993ace3900556
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2024-04-15 23:18:49 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <div className="pages"></div>
+      <Routes>
+        <Route path="/" />
+          
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
