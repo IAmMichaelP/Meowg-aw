ca336e2d4a2dc3685a27be118df05062be1279a3
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2024-04-20 22:19:27 +0800

    connect the css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './assets/css/main.css';
 import './assets/css/gallery.css';
 import './assets/css/shop.css';
+import { useEffect, useState } from 'react';
+
+// Favicons
+import faviconPng from './assets/img/favicon.png';
+import appleTouchIconPng from './assets/img/apple-touch-icon.png';
+
+// Font Awesome
+import 'font-awesome/css/font-awesome.min.css';
+
+// Additional Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+// Icons dependencies
+import 'boxicons/css/boxicons.min.css';
+import 'remixicon/fonts/remixicon.css';
+import 'font-awesome/css/font-awesome.min.css';
+import WebFont from 'webfontloader';
 
 // pages & components
 import Home from './pages/Home';
@@ -11,6 +29,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
+  
   return (
     <div className="App">
       <BrowserRouter>
