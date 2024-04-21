5e7a3ec3396cefc458d09de05113b8e84f14f717
Kzlyr Shaira Manejo <kmanejo@up.edu.ph>
2024-04-21 19:32:23 +0800

    update gallery

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -22,12 +22,11 @@ function displayStrayImages() {
 
         <div class="p-4">
           <h5><a href="#" class="text-dark stray-detail-link" data-stray='${JSON.stringify(stray)}'>${stray.name}</a></h5>
-          <p class="small text-muted mb-0">${stray.gender}</p>
-          <p class="small text-muted mb-0">STATUS: ${stray.status}</p>
+          <p class="small text-muted mb-0">Breed: ${stray.breed}</p>
 
           <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
-            <p class="small mb-0"><i class="fa fa-picture-o mr-2"></i><span class="font-weight-bold">${stray.breed.toUpperCase()}</span></p>
-            <div class="badge badge-${getBadgeColor(stray.animal)} px-3 rounded-pill font-weight-normal">${getBadgeText(stray.animal)}</div>
+            <p class="small mb-0"></i><span class="font-weight-bold">${stray.gender.toUpperCase()}</span></p>
+            <div class="badge badge-${getBadgeColor(stray.status)} px-3 rounded-pill font-weight-normal">${getBadgeText(stray.status)}</div>
           </div>
 
         </div>
@@ -55,20 +54,21 @@ function displayStrayImages() {
 }
 
 
-// for the modal
 function displayStrayDetails(stray) {
   const modalTitle = document.querySelector('#strayModalLabel');
   modalTitle.textContent = `Meet ${stray.name}`;
   var parsedImg = stray.imgData.toString('base64');
+
+  const submitButton = `<button class="adoptButton" type='submit'>Adopt ${stray.name}</button>`
 
   const strayDetailsContent = document.querySelector('#strayDetailsContent');
   // Clear existing content
   strayDetailsContent.innerHTML = '';
 
   // Populate modal with stray details
-  const strayDetailContent = `
+  let strayDetailContent = `
 
-    <img src="data:image/png; base64,${parsedImg}" alt="${stray.name}" class="gallery-img img-fluid card-img-top">
+    <img src="data:image/png; base64,${parsedImg}" alt="${stray.name}" class="gallery-img-modal img-fluid card-img-top">
 
     <h5>Name: ${stray.name}</h5>
     <p>Status: ${stray.status}</p>
@@ -94,7 +94,58 @@ function displayStrayDetails(stray) {
     <p class="strayDetails"><b>Vaccinated:</b> ${stray.vaccinated}</p>
     <p class="strayDetails"><b>Surrender Date:</b> ${stray.surrenderDate}</p><br>
   `;
-  strayDetailsContent.innerHTML = strayDetailContent;
+
+  // checks the display when a user is logged in
+  if (user) {
+    if(stray.status != 'evaluation for adoption ongoing') {
+      const parsedUser = JSON.parse(user);
+      strayDetailContent += `
+      <div class="adoptionForm">
+        <h3>Interested in adopting ${stray.name}?</h3>
+
+        <form action="/adopt" method="POST">
+          <input type="hidden" name="strayId" value="${stray._id}">
+          <label for="name">Full Name</label>
+          <input type="text" id="name" name="fullname" required>
+          <input type="hidden" name="email" value="${parsedUser.email}">
+          
+          <label for="reason">Reason for Adoption</label>
+          <input type="text" id="reason" name="reason" required>
+
+          <label for="interviewModes">Preferred Interview Mode:</label>
+            <div id="interviewModes">
+              <input type="radio" id="f2f" name="interviewMode" value="face-to-face" required>
+              <label for="f2f">Face-to-Face</label><br>
+              <input type="radio" id="zoom" name="interviewMode" value="zoom" required>
+              <label for="zoom">Zoom</label>
+            </div>
+
+          <label for="schedule">Preferred Interview Schedule:</label>
+          <input type="date" id="schedule" name="interviewSchedule" required>
+          <button class="adoptButton">Adopt ${stray.name}</button>
+        </form>
+      </div>
+      `;
+
+      strayDetailsContent.innerHTML = strayDetailContent;
+    } else {
+      strayDetailsContent.innerHTML = strayDetailContent;
+    }
+  } 
+  else {
+    // modal content
+    if(stray.status != 'evaluation for adoption ongoing') {
+      strayDetailsContent.innerHTML = '<form class="">' + strayDetailContent + submitButton + '</form>';
+      // handles the adoption when adopt button is clicked
+      const adoptButton = document.querySelector('.adoptButton');
+      adoptButton.addEventListener('click', async (e) => {
+        e.preventDefault();
+        openPopup('login-popup');
+      });
+    } else {
+      strayDetailsContent.innerHTML = strayDetailContent;
+    }
+  }
 
   // Show the modal
   $('#strayModal').modal('show'); // Use jQuery to show the modal
@@ -106,28 +157,28 @@ displayStrayImages();
 
 
 // Helper function to get badge color based on type
-function getBadgeColor(breed) {
-  switch (breed.toLowerCase()) {
-    case 'dog':
-      return 'danger';
-    case 'cat':
+function getBadgeColor(status) {
+  switch (status.toLowerCase()) {
+    case 'evaluation for adoption ongoing':
+       return 'danger';
+    case 'available for adoption':
       return 'success';
     // Add more cases
     default:
-      return 'primary';
+      return 'danger';
   }
 }
 
 // Helper function to get badge text based on type
-function getBadgeText(breed) {
-  switch (breed.toLowerCase()) {
-    case 'dog':
-      return 'Dog';
-    case 'cat':
-      return 'Cat';
+function getBadgeText(status) {
+  switch (status.toLowerCase()) {
+    case 'evaluation for adoption ongoing':
+      return 'Not available';
+    case 'available for adoption':
+      return 'available';
     // Add more cases
     default:
-      return 'Animal';
+      return 'available';
   }
 }
 
@@ -176,6 +227,8 @@ function showStrayData(category, color, sex) {
 
   filteredStrayData.forEach(stray => {
     var parsedImg = stray.imgData.toString('base64');
+
+    console.log('working');
 
     // Create gallery item
     const galleryItem = document.createElement('div');
