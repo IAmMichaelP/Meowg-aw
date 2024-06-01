0ba6361c7b871880676462c0fad8fdb00d697b50
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2024-06-01 16:53:45 +0800

    Update admin-modal.js

--- public/assets/js/admin-modal.js
+++ public/assets/js/admin-modal.js
@@ -19,27 +19,6 @@
 //         }
 //     };
 // }
-function closePopup(popupId) {
-    var popup = document.getElementById(popupId);
-    var backdrop = document.querySelector('.modal-backdrop'); // Assuming modal backdrop has this class
-  
-    // Animate modal close
-    popup.querySelector('.popup-content').style.opacity = '0';
-    popup.querySelector('.popup-content').style.transform = 'translateY(-50px)';
-    
-    setTimeout(function () {
-        popup.classList.remove('show');
-        popup.style.display = "none";
-        
-        // Remove the backdrop if it exists
-        if (backdrop) {
-            backdrop.parentNode.removeChild(backdrop);
-        }
-        
-        // Optionally remove the 'modal-open' class from the body if it exists
-        document.body.classList.remove('modal-open');
-    }, 500);
-  }
 
 function editFaqs(faqs) {
     console.log("reading edit faqs");
