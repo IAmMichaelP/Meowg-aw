91a2fb043d2d94b9409614ce99ee265a19897378
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2024-06-22 20:12:20 +0800

    fixed public folder errors for vercel deployment

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.set('view engine', 'ejs');
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // middleware and static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
